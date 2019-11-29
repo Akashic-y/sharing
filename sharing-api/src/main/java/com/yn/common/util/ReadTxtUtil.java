313a7c00990173c164463165bb5def4a41012228
yening <ye-ning@szreach.com>
2019-11-29 16:24:03 +0800

    ..

--- sharing-api/src/main/java/com/yn/common/util/ReadTxtUtil.java
+++ sharing-api/src/main/java/com/yn/common/util/ReadTxtUtil.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ReadTxtUtil {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         readAndWriteFile();
     }
 
