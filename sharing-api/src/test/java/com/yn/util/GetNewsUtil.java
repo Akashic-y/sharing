12f40c2b8a5328d94b583b976d8e1ea0ebc479d4
yening <ye-ning@szreach.com>
2020-03-27 17:06:39 +0800

    抽取方法

--- sharing-api/src/test/java/com/yn/util/GetNewsUtil.java
+++ sharing-api/src/test/java/com/yn/util/GetNewsUtil.java
@@ -15,9 +15,23 @@ import java.util.regex.Pattern;
 
 public class GetNewsUtil {
 
+    //文件保存路径
+    private static String path = "D:\\";
+
+    //请求地址
+    private static String url = "tr";
+
+    //用户名
+    private static String open_id = "";
+
+    //循环次数
+    private static int timer = 99;
+
     public static void main(String[] args) {
 //        getNewPeopleList();
-        getPeopleNewList();
+//        getPeopleNewList();
+//        getAllCompanyNews();
+        getAllCompanyNotice();
     }
 
     /**
@@ -26,44 +40,8 @@ public class GetNewsUtil {
      * @Date 14:51 2020/3/12 0012
      */
     public static void getNewPeopleList() {
-        String path = "D:\\newPeople.txt";
-        String fin = "D:\\newPeopleByDate.txt";
-        File f = new File(path);
-        new File(fin);
-        Map<String,String> param = new HashMap<>();
-        param.put("open_id","huang-bin");
-        param.put("co_id","10001");
-        param.put("news_type","132c8e7757a044968e6ec7fbcc6e423c");
-        param.put("plate_pid","132c8e7757a044968e6ec7fbcc6e423c");
-        param.put("plate_id","b249fbac2cc44a50944feb9dcc6a279d");
-        int i = 1;
-        try {
-            FileWriter fw = new FileWriter(path);
-            BufferedWriter bw = new BufferedWriter(fw);
-            FileWriter fw2 = new FileWriter(fin);
-            BufferedWriter bw2 = new BufferedWriter(fw2);
-            for (;i < 99; i++) {
-                param.put("pageNumber",i+"");
-                String rs = HttpKit.doPost("http://mp.wexcxc.com/sys/qywx/news/list", param);
-                if(rs.contains("没有数据")){
-                    break;
-                }
-//                System.out.println(rs);
-                List<String> list = getMatcher(rs.trim(), "h4");
-                List<String> time = getMatcher(rs.trim(), "span");
-                for (int k = 0; k < list.size(); k++) {
-                    bw2.write(list.get(k) + "---------------------" + time.get(k));
-                    bw2.newLine();
-                }
-                bw.write(rs.trim());
-                bw.flush();
-                bw2.flush();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("绝对路径--------------------" + f.getAbsolutePath());
-        System.out.println("一共-------" + (i-1));
+        getNewByType("132c8e7757a044968e6ec7fbcc6e423c","132c8e7757a044968e6ec7fbcc6e423c",
+                "b249fbac2cc44a50944feb9dcc6a279d","newStaff");
     }
 
     /**
@@ -72,25 +50,50 @@ public class GetNewsUtil {
      * @Date 14:51 2020/3/12 0012
      */
     public static void getPeopleNewList() {
-        String path = "D:\\peopleNews.txt";
-        String fin = "D:\\peopleNewsByDate.txt";
-        File f = new File(path);
+        getNewByType("24d324e25105437688d5d073bc7a9e14","24d324e25105437688d5d073bc7a9e14",
+                "ba0be40d7f4f4e8a830f60a1b699667c","peopleNews");
+    }
+
+    /**
+     * @Desc 获取所有company新闻
+     * @Author yn
+     * @Date 16:51 2020/3/27 0027
+     */
+    public static void getAllCompanyNews(){
+        getNewByType("132c8e7757a044968e6ec7fbcc6e423c","132c8e7757a044968e6ec7fbcc6e423c",
+                "","allNews");
+    }
+
+    /**
+     * @Desc 获取company公告
+     * @Author yn
+     * @Date 16:52 2020/3/27 0027
+     */
+    public static void getAllCompanyNotice() {
+        getNewByType("24d324e25105437688d5d073bc7a9e14","24d324e25105437688d5d073bc7a9e14",
+                "","allNotice");
+    }
+
+    public static void getNewByType(String news_type,String plate_pid,String plate_id,String fileName) {
+        String filePath = path +fileName+".txt";
+        String fin = path +fileName+"ByDate.txt";
+        File f = new File(filePath);
         new File(fin);
         Map<String,String> param = new HashMap<>();
-        param.put("open_id","huang-bin");
+        param.put("open_id",open_id);
         param.put("co_id","10001");
-        param.put("news_type","24d324e25105437688d5d073bc7a9e14");
-        param.put("plate_pid","24d324e25105437688d5d073bc7a9e14");
-        param.put("plate_id","ba0be40d7f4f4e8a830f60a1b699667c");
+        param.put("news_type",news_type);
+        param.put("plate_pid",plate_pid);
+        param.put("plate_id",plate_id);
         int i = 1;
         try {
-            FileWriter fw = new FileWriter(path);
+            FileWriter fw = new FileWriter(filePath);
             BufferedWriter bw = new BufferedWriter(fw);
             FileWriter fw2 = new FileWriter(fin);
             BufferedWriter bw2 = new BufferedWriter(fw2);
-            for (;i < 99; i++) {
+            for (;i < timer; i++) {
                 param.put("pageNumber",i+"");
-                String rs = HttpKit.doPost("http://mp.wefddfwwev.com/sys/qywx/news/list", param);
+                String rs = HttpKit.doPost(url, param);
                 if(rs.contains("没有数据")){
                     break;
                 }
